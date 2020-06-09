3d513fbcafc147c5f2eb51845f1795565aa79115
mistahpig <dspark410@gmail.com>
2020-06-08 19:38:49 -0700

    adds formatting, and badges

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const questionsPrompt = [
     },
     {
         type: "input",
+        message: "Enter your badge code. Use this format: [![Build Status](https://travis-ci.com/username/projectname.svg?branch=master)](https://travis-ci.com/username/projectname)",
+        name: "badge"
+    },
+    {
+        type: "input",
         message: "Enter the description of your project",
         name: "description"
     },
@@ -19,84 +24,127 @@ const questionsPrompt = [
     },
     {
         type: "input",
-        message: "Installation Guide",
+        message: "How does a user install this application?",
         name: "installation"
     },
     {
         type: "input",
-        message: "Usage Guide",
+        message: "How does a user run this application?",
         name: "usage"
     },
     {
         type: "input",
-        message: "Enter License",
+        message: "What license would you like to use? Type 'none' if you do not wish to use a license",
         name: "license"
     },
     {
         type: "input",
-        message: "Enter contributing authors",
-        name: "authors"
+        message: "Would you like users to be able to contribute to this project?",
+        name: "contribute"
     },
     {
         type: "input",
-        message: "Enter tests",
+        message: "Would you like to write tests for your application?",
         name: "tests"
     },
+    {
+        type: "input",
+        message: "Enter the url of your GitHub profile picture. Use this format: ![alttext](url)",
+        name: "profile"
+    },
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your GitHub email?",
+        name: "email"
+    },
 ]
+
 
 inquirer.prompt(questionsPrompt)
 
     .then(function (response) {
-        fs.appendFile("README.md", `# ${response.title} \n`, function(err) {
+        fs.appendFileSync("README.md", `# ${response.title} \n`, function(err) {
             if (err) {
                 return console.log(err)
             }
             console.log("Success!")
         })
 
-        fs.appendFile("README.md", `# ${response.description} \n`, function(err) {
+        fs.appendFileSync("README.md", `${response.badge} \n\n`, function(err) {
             if (err) {
                 return console.log(err)
             }
             console.log("Success!")
         })
 
-        fs.appendFile("README.md", `# ${response.tableofcontents} \n`, function(err) {
+        fs.appendFileSync("README.md", `## Description \n${response.description} \n\n`, function(err) {
             if (err) {
                 return console.log(err)
             }
             console.log("Success!")
         })
 
-        fs.appendFile("README.md", `# ${response.installation} \n`, function(err) {
+        fs.appendFileSync("README.md", `## Table of Contents \n- ${response.tableofcontents.split(", ").join("\n" + "- " )} \n\n`, function(err) {
             if (err) {
                 return console.log(err)
             }
             console.log("Success!")
         })
 
-        fs.appendFile("README.md", `# ${response.usage} \n`, function(err) {
+        fs.appendFileSync("README.md", `## Installation \n${response.installation} \n\n`, function(err) {
             if (err) {
                 return console.log(err)
             }
             console.log("Success!")
         })
 
-        fs.appendFile("README.md", `# ${response.license} \n`, function(err) {
+        fs.appendFileSync("README.md", `## Usage \n${response.usage} \n\n`, function(err) {
+            if (err) {
+                return console.log(err)
+            }
+            console.log("Success!")
+        })
+
+        fs.appendFileSync("README.md", `## License(s) \n${response.license} \n\n`, function(err) {
+            if (err) {
+                return console.log(err)
+            }
+            console.log("Success!")
+        })
+
+        fs.appendFileSync("README.md", `## Contributing \n${response.contribute} \n\n`, function(err) {
             if (err) {
                 return console.log(err)
             }
             console.log("Success!")
         })
         
-        fs.appendFile("README.md", `# ${response.authors} \n`, function(err) {
+        fs.appendFileSync("README.md", `## Tests \n${response.tests} \n\n`, function(err) {
             if (err) {
                 return console.log(err)
             }
             console.log("Success!")
         })
 
-        fs.appendFile("README.md", `# ${response.tests} \n`, function(err) {
+        fs.appendFileSync("README.md", `## Questions \n${response.profile} \n\n`, function(err) {
+            if (err) {
+                return console.log(err)
+            }
+            console.log("Success!")
+        })
+        fs.appendFileSync("README.md", `GitHub: ${response.username} \n\n`, function(err) {
+            if (err) {
+                return console.log(err)
+            }
+            console.log("Success!")
+        })
+
+        fs.appendFileSync("README.md", `Email: ${response.email}`, function(err) {
             if (err) {
                 return console.log(err)
             }
